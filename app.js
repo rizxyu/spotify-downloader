887ee28202f7dade2e085f93cce4a22b0cee4b7a
Rizky Kang Turu <88314302+rizxyu@users.noreply.github.com>
2023-12-22 19:05:11 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,40 @@
+function downloadSong() {
+  const urlInput = document.getElementById('urlInput').value;
+  if (urlInput.trim() === '') {
+    alert('Masukkan URL lagu Spotify terlebih dahulu.');
+    return;
+  }
+
+  // Gantilah dengan logika pengunduhan lagu dari API sesuai kebutuhan.
+  // Misalnya, Anda dapat menggunakan fetch() atau library seperti axios.
+
+  // Contoh fetch():
+  fetch(`/api/downloader?endpoint=spotify&text=${encodeURIComponent(urlInput)}`)
+    .then(response => response.json())
+    .then(data => {
+      displayResult(data);
+    })
+    .catch(error => {
+      console.error('Error:', error);
+      alert('Terjadi kesalahan dalam mengunduh lagu.');
+    });
+}
+
+function displayResult(result) {
+  const resultContainer = document.getElementById('resultContainer');
+  resultContainer.innerHTML = `
+    <p><strong>Judul:</strong> ${result.judul}</p>
+    <p><strong>Artis:</strong> ${result.artis}</p>
+    <p><strong>Album:</strong> ${result.album}</p>
+    <p><strong>Rilis:</strong> ${result.rilis}</p>
+    <img src="${result.thumb}" alt="Thumbnail" style="max-width: 100%; height: auto;">
+    <audio controls>
+      <source src="${result.audio}" type="audio/mpeg">
+      Browser Anda tidak mendukung tag audio.
+    </audio>
+  `;
+
+  // Tampilkan hasil section setelah mendapatkan hasil.
+  const resultSection = document.querySelector('.result-section');
+  resultSection.style.display = 'block';
+}
